3cc56fe6ee7170f0e0a118f79f673092959b28a5
cgsdfc <cgsdfc@126.com>
2020-09-27 16:57:44 +0800

    AddTest.

--- test/argparse-utils_test.cc
+++ test/argparse-utils_test.cc
@@ -5,9 +5,53 @@
 namespace argparse {
 namespace {
 
-TEST(Any, MakeAny) {}
+struct CtorOverload {
+  enum {
+    kCtorDouble,
+    kCtorInt,
+    kCtorChar,
+  };
+  int called_ctor;
+  explicit CtorOverload(double) : called_ctor(kCtorDouble) {}
+  explicit CtorOverload(int) : called_ctor(kCtorInt) {}
+  explicit CtorOverload(char) : called_ctor(kCtorChar) {}
+};
 
-TEST(Any, AnyCast) {}
+TEST(Any, MakeAnyCallsTheCorrectCtor) {
+  auto val = MakeAny<CtorOverload>(double());
+  EXPECT_TRUE(AnyCast<CtorOverload>(*val).called_ctor ==
+              CtorOverload::kCtorDouble);
+}
+
+TEST(Any, AnyImplHasCorrectType) {
+  auto val = MakeAny<int>(1);
+  EXPECT_TRUE(val->GetType() == typeid(int));
+}
+
+TEST(Any, AnyCastMoveFormWorks) {
+  auto val = MakeAny<int>(1);
+  EXPECT_TRUE(AnyCast<int>(std::move(val)) == 1);
+}
+
+TEST(Any, AnyCastConstFormWorks) {
+  auto val = MakeAny<int>(1);
+  EXPECT_TRUE(AnyCast<int>(*val) == 1);
+}
+
+struct MoveOnlyType {
+  explicit MoveOnlyType(int val) { this->val = val; }
+  MoveOnlyType(MoveOnlyType&&) = default;
+  bool operator==(const MoveOnlyType& that) const { return val == that.val; }
+  int val;
+};
+
+static_assert(std::is_move_constructible<MoveOnlyType>{});
+static_assert(!std::is_copy_constructible<MoveOnlyType>{});
+
+TEST(Any, AnyCanWrapMoveOnlyType) {
+  auto val = MakeAny<MoveOnlyType>(1);
+  EXPECT_TRUE(AnyCast<MoveOnlyType>(std::move(val)).val == 1);
+}
 
 TEST(Result, WhenDefaultConstructedTheStateIsCorrect) {
   Result<int> res;
@@ -65,16 +109,6 @@ TEST(Result, AssignmentWorksAsSetValue) {
   EXPECT_TRUE(res.has_value());
   EXPECT_TRUE(res.get_value() == 1);
 }
-
-struct MoveOnlyType {
-  explicit MoveOnlyType(int val) { this->val = val; }
-  MoveOnlyType(MoveOnlyType&&) = default;
-  bool operator==(const MoveOnlyType& that) const { return val == that.val; }
-  int val;
-};
-
-static_assert(std::is_move_constructible<MoveOnlyType>{});
-static_assert(!std::is_copy_constructible<MoveOnlyType>{});
 
 TEST(Result, WorksForMoveOnlyType) {
   Result<MoveOnlyType> res;
