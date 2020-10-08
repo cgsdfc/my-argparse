85f277777d37fbb2445587d5ea31096d9d7fcafe
cgsdfc <cgsdfc@126.com>
2020-10-08 16:33:23 +0800

    Add gflags.

--- src/argparse-gflags-parser.cc
+++ src/argparse-gflags-parser.cc
@@ -1,29 +1,18 @@
 // Copyright (c) 2020 Feng Cong
-// 
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-#include <argp.h>
+#include <gflags/gflags.h>
 
 #include "argparse/internal/argparse-internal.h"
 
 namespace argparse {
 namespace internal {
 
-class ArgumentNamespace {
-public:
-    virtual ~ArgumentNamespace() {}
-    // virtual Argument* 
-};
-
-class SubCommandNamespace {
-
-};
-
-class ArgpArgumentParser : public ArgumentParser {
+class GflagsArgumentParser : public ArgumentParser {
  public:
-  void AddArgument(Argument* arg) override {}
-
+ private:
 };
 
 }  // namespace internal
