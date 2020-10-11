a4f33dd2d53bb804f763d0255ea1286e2770f7d8
cgsdfc <cgsdfc@126.com>
2020-10-11 22:14:25 +0800

    Prepare to refact Result, not yet..

---
+++ include/argparse/argparse-conversion-result.h
@@ -0,0 +1,31 @@
+// Copyright (c) 2020 Feng Cong
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+#pragma once
+
+#include "argparse/internal/argparse-result.h"
+
+namespace argparse {
+
+class ConversionResult {
+ public:
+  explicit ConversionResult(internal::Result result)
+      : result_(std::move(result)) {}
+
+ private:
+  internal::Result result_;
+};
+
+inline ConversionResult ConversionFailure(std::string error) {
+  return ConversionResult(internal::Result(std::move(error)));
+}
+
+template <typename T>
+ConversionResult ConversionSuccess(T&& value) {
+  auto any = internal::MakeAny<std::decay_t<T>>(std::forward<T>(value));
+  return ConversionResult(internal::Result(std::move(any)));
+}
+
+}  // namespace argparse
--- include/argparse/internal/argparse-result.h
+++ include/argparse/internal/argparse-result.h
@@ -5,6 +5,9 @@
 
 #pragma once
 
+#include <memory>
+#include <string>
+
 #include "argparse/internal/argparse-any.h"
 #include "argparse/internal/argparse-port.h"
 
@@ -13,10 +16,40 @@ namespace internal {
 
 // Result is a union of a arbitrary value (type-erased) and an error message.
 class Result {
-public:
+ public:
+  explicit Result(std::unique_ptr<Any> value) : value_(std::move(value)) {}
+  explicit Result(std::string error)
+      : error_(std::make_unique<std::string>(std::move(error))) {}
 
-private:
-    std::unique_ptr<Any> value_;
+  Result() = default;
+
+  bool HasValue() const { return !!value_; }
+  bool HasError() const { return !!error_; }
+  bool IsEmpty() const { return !HasValue() && !HasError(); }
+
+  const std::string& error() const{
+    ARGPARSE_DCHECK(HasError());
+    return *error_;
+  }
+
+  const Any* value() const {
+    ARGPARSE_DCHECK(HasValue());
+    return value_.get();
+  }
+
+  std::unique_ptr<Any> TakeValue() {
+    ARGPARSE_DCHECK(HasValue());
+    return std::move(value_);
+  }
+
+  std::unique_ptr<std::string> TakeError() {
+    ARGPARSE_DCHECK(HasError());
+    return std::move(error_);
+  }
+
+ private:
+  std::unique_ptr<Any> value_;
+  std::unique_ptr<std::string> error_;
 };
 
 }  // namespace internal
