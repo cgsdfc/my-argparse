8297edf866ece2831320be73e2b4efad83c11ef7
cgsdfc <cgsdfc@126.com>
2020-10-25 19:41:34 +0800

    Add state.

--- argparse/internal/argparse-argument-controller.cc
+++ argparse/internal/argparse-argument-controller.cc
@@ -12,5 +12,48 @@ ArgumentController::ArgumentController()
     : container_(new ArgumentContainer),
       parser_(ArgumentParser::CreateDefault()) {}
 
+void ArgumentController::EnsureInActiveState(const char* func) const {
+  ARGPARSE_DCHECK_F(state_ == kActiveState,
+                    "Method %s must be called in Active state", func);
+}
+
+void ArgumentController::EnsureInFrozenState() {
+  ARGPARSE_DCHECK_F(
+      state_ != kShutDownState,
+      "No method other than destructor should be called after shutdown");
+  if (state_ == kFrozenState) return;
+  TransmitToFrozenState();
+}
+
+void ArgumentController::TransmitToFrozenState() {
+  EnsureInActiveState(__func__);
+  bool rv = parser_->Initialize(container_.get());
+  ARGPARSE_DCHECK_F(rv, "Cannot initialize the ArgumentParser");
+  state_ = kFrozenState;
+}
+
+void ArgumentController::AddArgument(std::unique_ptr<Argument> arg) {
+  EnsureInActiveState(__func__);
+  return container_->GetMainHolder()->AddArgument(std::move(arg));
+}
+
+ArgumentGroup* ArgumentController::AddArgumentGroup(std::string title) {
+  EnsureInActiveState(__func__);
+  return container_->GetMainHolder()->AddArgumentGroup(std::move(title));
+}
+
+bool ArgumentController::ParseKnownArgs(ArgArray args,
+                                        std::vector<std::string>* out) {
+  EnsureInFrozenState();
+  return parser_->ParseKnownArgs(args, out);
+}
+
+void ArgumentController::Shutdown() {
+  if (state_ == kShutDownState) return;
+  // Must delete container first.
+  container_.reset();
+  parser_.reset();
+}
+
 }  // namespace internal
 }  // namespace argparse
--- argparse/internal/argparse-argument-controller.h
+++ argparse/internal/argparse-argument-controller.h
@@ -18,33 +18,41 @@ class ArgumentController final {
   ArgumentController();
 
   // Methods forwarded from ArgumentContainer.
-  void AddArgument(std::unique_ptr<Argument> arg) {
-    return container_->GetMainHolder()->AddArgument(std::move(arg));
-  }
-  ArgumentGroup* AddArgumentGroup(std::string title) {
-    return container_->GetMainHolder()->AddArgumentGroup(std::move(title));
-  }
+  void AddArgument(std::unique_ptr<Argument> arg);
+
+  ArgumentGroup* AddArgumentGroup(std::string title);
+
   SubCommandGroup* AddSubCommandGroup(std::unique_ptr<SubCommandGroup> group) {
+    EnsureInActiveState(__func__);
     return nullptr;
   }
 
-  // Methods forwarded from ArgumentParser.
-  ArgumentParser* GetOptionsListener() { return parser_.get(); }
-
-  bool ParseKnownArgs(ArgArray args, std::vector<std::string>* out) {
-    bool rv = parser_->Initialize(container_.get());
-    ARGPARSE_DCHECK_F(rv, "Cannot initialize the ArgumentParser");
-    return parser_->ParseKnownArgs(args, out);
+  // TODO: use OptionsListenr.
+  ArgumentParser* GetOptionsListener() {
+    EnsureInActiveState(__func__);
+    return parser_.get();
   }
+
+  bool ParseKnownArgs(ArgArray args, std::vector<std::string>* out);
 
   // Clean all the memory of this object, after that no methods other than dtor
   // should be invoked.
-  void Shutdown() {
-    container_.reset();
-    parser_.reset();
-  }
+  void Shutdown();
 
  private:
+  enum State {
+    kActiveState,    // In this state, arguments can be added to us.
+    kFrozenState,    // In this state, no argument can be added, but parse() can
+                     // be called.
+    kShutDownState,  // In this state, nothing can be do with it, waiting for
+                     // dtor call.
+  };
+
+  void EnsureInActiveState(const char* func) const;
+  void EnsureInFrozenState();
+  void TransmitToFrozenState();
+
+  State state_ = kActiveState;
   std::unique_ptr<ArgumentContainer> container_;
   std::unique_ptr<ArgumentParser> parser_;
 };
