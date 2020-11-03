1aea660fd735a4abdda8eb5ab9b070f880a29c7c
cgsdfc <cgsdfc@126.com>
2020-11-03 18:38:39 +0800

    Add logging module.

---
+++ argparse/internal/argparse-logging.cc
@@ -0,0 +1,16 @@
+// Copyright (c) 2020 Feng Cong
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+#include "argparse/internal/argparse-logging.h"
+
+namespace argparse {
+namespace internal {
+namespace logging_internal {
+
+
+
+}  // namespace logging_internal
+}  // namespace internal
+}  // namespace argparse
---
+++ argparse/internal/argparse-logging.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2020 Feng Cong
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+#pragma once
+
+#include "absl/base/log_severity.h"
+#include "absl/strings/str_format.h"
+
+#define ARGPARSE_INTERNAL_LOG()
+namespace argparse {
+namespace internal {
+namespace logging_internal {
+
+}  // namespace logging_internal
+}  // namespace internal
+}
